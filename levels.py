b2150a16f94f17546310df0799ae13fe322a0d99
wilsaj <wilson.andrew.j+github@gmail.com>
2012-01-27 22:09:21 -0600

    pull widgets out of main.py and into their own module

--- levels.py
+++ levels.py
@@ -1,12 +1,10 @@
 """module that holds all the levels"""
 
-
-import main
-
+from widgets import AimLine, BlackHole, GoalPoint, Rocket, Shot, Stars
 
 class First(object):
     def load(self, fling_board):
-        goal_point = main.GoalPoint(pos=(50, 50))
+        goal_point = GoalPoint(pos=(250, 250))
         fling_board.add_widget(goal_point)
         fling_board.goal_points.append(goal_point)
 
--- main.py
+++ main.py
@@ -1,143 +1,20 @@
 #!/usr/bin/env python
-from functools import partial
 import itertools
 import math
-from random import random
 
 import kivy
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.core.window import Window
 from kivy.lang import Builder
-from kivy.graphics import Color, Ellipse, Line
-from kivy.properties import BooleanProperty, ListProperty, NumericProperty
 from kivy.vector import Vector
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
-from kivy.uix.button import Button
 
+from widgets import AimLine, BlackHole, GoalPoint, Rocket, Shot, Stars
+import levels
 
 kivy.require('1.0.9')
-
-
-class AimLine(Widget):
-    start_pt = ListProperty([0, 0])
-    end_pt = ListProperty([0, 0])
-
-    def __init__(self, start_pt, **kwargs):
-        super(AimLine, self).__init__(**kwargs)
-        self.start_pt = start_pt
-        self.end_pt = start_pt
-
-
-class BlackHole(Widget):
-    d = NumericProperty(50.)
-    mass = NumericProperty(50.)
-
-    def __init__(self, **kwargs):
-        super(BlackHole, self).__init__(**kwargs)
-
-    def collide_point(self, x, y):
-        if (Vector(x, y) - Vector(self.pos)).length() < self.d / 2.:
-            return True
-
-
-class GoalPoint(Widget):
-    r = NumericProperty(10.)
-
-    def __init__(self, **kwargs):
-        super(GoalPoint, self).__init__(**kwargs)
-
-    def collide_point(self, x, y):
-        if (Vector(x, y) - Vector(self.pos)).length() < self.r:
-            return True
-
-
-class Rocket(Widget):
-    l = NumericProperty(20.)
-    w = NumericProperty(8.)
-    tip_l = NumericProperty(1.5)
-    tip_w = NumericProperty(1.5)
-    motion_v = ListProperty([0, 0])
-    boost = BooleanProperty(False)
-    quad_points = ListProperty([0, 0, 0, 0, 0, 0, 0, 0])
-    # tip_points = ListProperty([0, 0, 0, 0, 0, 0])
-
-    def __init__(self, **kwargs):
-        super(Rocket, self).__init__(**kwargs)
-
-    def update_points(self):
-        m_v = Vector(self.motion_v)
-        l_v = m_v.normalize() * self.l / 2.
-        # orthogonal vector
-        o_v = m_v.normalize().rotate(90) * self.w / 2.
-        self.quad_points = [self.x - (l_v + o_v)[0], self.y - (l_v + o_v)[1],
-                            self.x + (l_v - o_v)[0], self.y + (l_v - o_v)[1],
-                            self.x + (l_v + o_v)[0], self.y + (l_v + o_v)[1],
-                            self.x - (l_v - o_v)[0], self.y - (l_v - o_v)[1],
-                            ]
-
-        # self.tip_points = [
-        #     self.x + (l_v * self.tip_l)[0], self.y + (l_v * self.tip_l)[1],
-        #     self.x + (l_v + (o_v + Vector(self.tip_w, 0)))[0], self.y + (l_v + (o_v + Vector(self.tip_w, 0)))[1],
-        #     self.x + (l_v - (o_v - Vector(self.tip_w, 0)))[0], self.y + (l_v + (o_v - Vector(self.tip_w, 0)))[1],
-        #     ]
-
-    def move(self):
-        if self.boost:
-            m_v = Vector(self.motion_v)
-            self.motion_v = m_v + m_v.normalize()
-        self.x += self.motion_v[0]
-        self.y += self.motion_v[1]
-        self.update_points()
-
-    def gravitate_towards(self, body):
-        m_v = Vector(self.motion_v)
-        g_v = Vector(body.pos) - Vector(self.pos)
-        self.motion_v = m_v + (g_v * 1. / g_v.length2()) * body.mass
-
-
-class Shot(Widget):
-    d = NumericProperty(10.)
-    mass = NumericProperty(1.)
-    motion_v = ListProperty([0, 0])
-
-    def __init__(self, motion_v, **kwargs):
-        super(Shot, self).__init__(**kwargs)
-        self.motion_v = motion_v
-
-    def move(self):
-        self.x += self.motion_v[0]
-        self.y += self.motion_v[1]
-
-    def gravitate_towards(self, body):
-        m_v = Vector(self.motion_v)
-        g_v = Vector(body.pos) - Vector(self.pos)
-        self.motion_v = m_v + (g_v * 1. / g_v.length2()) * body.mass
-
-
-class Stars(Widget):
-    points = ListProperty([(0, 0), (0, 0), (0, 0)])
-
-    def __init__(self, number_of_stars, **kwargs):
-        super(Stars, self).__init__(**kwargs)
-        Clock.schedule_once(partial(self.add_stars, number_of_stars), -1)
-
-    def add_stars(self, number_of_stars, dt):
-        width = self.parent.width
-        height = self.parent.height
-        self.points[0] = list(itertools.chain(*[
-                    (random() * width, random() * height)
-                    for i in xrange(number_of_stars)]))
-        self.points[1] = list(itertools.chain(*[
-                    (random() * width, random() * height)
-                    for i in xrange(number_of_stars / 3)]))
-        self.points[2] = list(itertools.chain(*[
-                    (random() * width, random() * height)
-                    for i in xrange(number_of_stars / 50)]))
-
-
-import levels
 
 
 class FlingBoard(Widget):
