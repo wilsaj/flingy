c5b41d5cf00336e4fd443b10dff643f06328d0e8
wilsaj <wilson.andrew.j+github@gmail.com>
2012-01-28 14:29:12 -0600

    two more levels

--- levels.py
+++ levels.py
@@ -1,5 +1,7 @@
 """module that holds all the levels"""
 
+from kivy.clock import Clock
+
 from widgets import AimLine, BlackHole, GoalPoint, Rocket, Shot, Stars
 
 
@@ -34,4 +36,58 @@ class LongShot(object):
             fling_board.goal_points.append(goal_point)
 
 
-levels = [First, LongShot]
+class TwoTimed(object):
+    name = "two, timed"
+
+    def load(self, fling_board):
+        top_height = fling_board.height * .7
+        bottom_height = fling_board.height * .3
+        left_x = fling_board.width * .4
+        right_x = fling_board.width * .6
+        seconds_til_switch = (top_height - bottom_height) / 60.
+
+        for start_x in (left_x, right_x):
+            goal_point = GoalPoint(pos=(start_x, top_height),
+                                   velocity_y=-1)
+            Clock.schedule_interval(self.change_direction(goal_point),
+                                    seconds_til_switch)
+            fling_board.add_widget(goal_point)
+            fling_board.goal_points.append(goal_point)
+
+
+    def change_direction(self, goal_point):
+        def change_func(dt):
+            goal_point.velocity_y *= -1
+
+        return change_func
+
+
+class TwoTimedTwo(object):
+    name = "two, timed: part II"
+
+    def load(self, fling_board):
+        top_height = fling_board.height * .7
+        bottom_height = fling_board.height * .3
+        left_x = fling_board.width * .4
+        right_x = fling_board.width * .6
+        seconds_til_switch = (top_height - bottom_height) / 60.
+
+        for start_x, start_y, start_velocity_y in ((left_x, top_height, -1),
+                                                   (right_x, bottom_height, 1)):
+            goal_point = GoalPoint(pos=(start_x, start_y),
+                                   velocity_y=start_velocity_y)
+            Clock.schedule_interval(self.change_direction(goal_point),
+                                    seconds_til_switch)
+            fling_board.add_widget(goal_point)
+            fling_board.goal_points.append(goal_point)
+
+
+    def change_direction(self, goal_point):
+        def change_func(dt):
+            goal_point.velocity_y *= -1
+
+        return change_func
+
+
+
+levels = [First, LongShot, TwoTimed, TwoTimedTwo]
